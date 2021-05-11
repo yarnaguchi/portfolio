6808dbfdae89f8259e9ddf287ba9fa6008f877da
YamaguchiRyuta <steven_siro_vai@hotmail.co.jp>
2021-05-11 19:35:47 +0900

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import { SimpleTabs } from './components/MyTab'
 import { MiniDrawer } from './components/MiniVariantDrawer'
+// import { SimpleBottomNavigation } from './components/BottomNavigation'
 
 function App() {
   return (
--- src/components/MiniVariantDrawer.tsx
+++ src/components/MiniVariantDrawer.tsx
@@ -17,6 +17,7 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import { SimpleBottomNavigation } from './BottomNavigation'
 
 const drawerWidth = 240;
 
@@ -174,17 +175,7 @@ export function MiniDrawer() {
           arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
           donec massa sapien faucibus et molestie ac.
         </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <SimpleBottomNavigation />
       </main>
     </div>
   );
