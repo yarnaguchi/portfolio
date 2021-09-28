427d9480d2307b402f1697cb3b8f81c30d19a43c
YamaguchiRyuta <steven_siro_vai@hotmail.co.jp>
2021-09-28 17:46:32 +0900

    ReNew

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Getting Started with Create React App
-
 デプロイ結果 [Github Pages](https://yamaguchiryuta.github.io/portfolio/)
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-// import { SimpleTabs } from './components/MyTab'
-import { MiniDrawer } from './components/MiniVariantDrawer'
-// import { SimpleBottomNavigation } from './components/BottomNavigation'
-
 function App() {
-  return (
-    // <SimpleTabs />
-    <MiniDrawer />
-  );
+  return <></>;
 }
 
 export default App;
