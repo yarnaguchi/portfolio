664664055a9b10f78890a942770746e10c9180d7
YamaguchiRyuta <steven_siro_vai@hotmail.co.jp>
2021-10-01 17:46:59 +0900

    Update workRecords.tsx

--- src/assets/workRecords.tsx
+++ src/assets/workRecords.tsx
@@ -2,14 +2,15 @@ import { WorkRecord } from "../interface/interface";
 
 export const workRecords: WorkRecord[] = [
   {
-    title: "RecordTitle001",
-    description: "RecordDescription001",
-    url: "https://yamaguchiryuta.github.io/my-gatsby-site/",
-    tags: ["tag001", "tag002"],
+    title: "MaterialDesign",
+    description: "MaterialDesign Web",
+    url: "https://yamaguchiryuta.github.io/material_design/",
+    tags: ["React", "Material-UI", "Typescript"],
   },
   {
-    title: "RecordTitle002",
-    description: "RecordDescription002",
-    url: "https://yamaguchiryuta.github.io/my-gatsby-site/",
+    title: "Flutter Web",
+    description: "Flutter Web版",
+    url: "https://fir-first-a8330.web.app/",
+    tags: ["Dart", "Flutter", "firebase"],
   },
 ];
