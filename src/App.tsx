2b44d04494f2fcbb595a09da896a68d53ba39191
YamaguchiRyuta <steven_siro_vai@hotmail.co.jp>
2021-10-01 10:10:47 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Page } from "./interface/interface";
 export const App: VFC<{}> = () => {
   const [page, setPage] = useState<Page>("works");
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen"> 
       <Header {...{ page, setPage }} />
       <Main {...{ page, setPage }} />
       <Footer />
